b7f2a11533156acf0e70850ac7e9f10d1e5ace33
Darkzero7 <chaiyanituporn@gmail.com>
2024-05-19 09:42:21 +0700

    js lab 27

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-// write code here
+let username =prompt("username : ");
+let password =prompt("password : ");
+if (username == '') {
+   alert("username is required");
+}else if (password == '') {
+   alert("password is required")
+}else {
+    if (username == 'admin' && password == '1234' || username == 'john' && password == 'qwerty' ) {
+       alert(`Hello ${username}`);
+    }else {
+       alert('invalid username or password')
+    }
+}
